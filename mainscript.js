79d3185a08ac129f61f23b1f7e420f93df7ec60a
Rithin Jose <rithinja@gmail.com>
2019-01-29 21:51:41 +0530

    js updated again

--- mainscript.js
+++ mainscript.js
@@ -1,4 +1,3 @@
-<script> var event = "welcome to techinical event"; function func1() { return
-(event); } console.log(func1()); </script> <script> var asthra =
-"rot13:nfg{wnin_sha}"; function func() { return (asthra); } console.log(func());
-</script>
+var event = "welcome to techinical event"; function func1() { return
+(event); } console.log(func1()); var asthra =
+"rot13:nfg{wnin_sha}"; function func() { return (asthra); } 
