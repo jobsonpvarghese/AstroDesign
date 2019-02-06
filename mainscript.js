e5d89298238e0e698a25613ea6d5d211420bbb75
Jobson P Varghese <jobsonvarghese1@gmail.com>
2019-02-06 12:06:15 +0530

    update

--- mainscript.js
+++ mainscript.js
@@ -1,3 +1,3 @@
 var event = "welcome to techinical event"; function func1() { return
-(event); } console.log(func1()); var asthra =
-"rot13:nfg{wnin_sha}"; function func() { return (asthra); } 
+(event); } console.log(func1())
+console.log("rot13:nfg{wnin_sha}");
