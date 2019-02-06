59ddc6b1d836eb40be0100c7bf32efc4639eda9a
Jobson P Varghese <jobsonvarghese1@gmail.com>
2019-02-06 11:52:54 +0530

    Add files via upload

--- mainscript.js
+++ mainscript.js
@@ -1,3 +1,3 @@
 var event = "welcome to techinical event"; function func1() { return
 (event); } console.log(func1()); var asthra =
-"rot13:nfg{wnin_sha}"; function func() { return (asthra); } console.log(func());
+"rot13:nfg{wnin_sha}"; function func() { return (asthra); } 
